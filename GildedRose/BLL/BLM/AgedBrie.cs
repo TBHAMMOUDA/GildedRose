0ee72cb20868980dcf46d9bd1ae5adf576e1a8e8
Taha Benhammouda <taha.hammouda@gmail.com>
2022-07-19 20:31:39 +0200

    Add the business logic

---
+++ GildedRose/BLL/BLM/AgedBrie.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace GildedRoseKata.BLL
+{
+    public class AgedBrie : ProductFactoryHandler
+    {
+
+        public override void DayPass(Item item)
+        {
+            item.SellIn--;
+            item.Quality++;
+            var cheks = ApplyCheks(item);
+
+            item.Quality = cheks.IsreachedMin ? 0 : item.Quality;
+            item.Quality = cheks.IsreachedMax ? 50 : item.Quality;
+        }
+    }
+}
