142480d2f51f46250dc922982736d8bd51e4d154
Taha Benhammouda <taha.hammouda@gmail.com>
2022-07-19 20:23:41 +0200

    Add rules check class

---
+++ GildedRose/BLL/Check.cs
@@ -0,0 +1,26 @@
+﻿
+namespace GildedRoseKata.BLL
+{
+    public class Check
+    {
+        private const int MaxQuality = 50;
+        private const int MinQuality = 0;
+        private const int _0Days = 0;
+        private const int _5Days = 5;
+        private const int _10Days = 10;
+        public bool SellInPassed { get; set; }
+        public bool IsLast5Days { get; set; }
+        public bool IsLast10Days { get; set; }
+        public bool IsreachedMin { get; set; }
+        public bool IsreachedMax { get; set; }
+
+        public Check(Item item)
+        {
+            SellInPassed = item.SellIn < _0Days;
+            IsLast5Days = item.SellIn < _5Days+1;
+            IsLast10Days = item.SellIn < _10Days + 1;
+            IsreachedMin = item.Quality <= MinQuality;
+            IsreachedMax = item.Quality > MaxQuality;
+        }
+    }  
+}
